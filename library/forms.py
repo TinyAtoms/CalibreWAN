83e471462bf8e4d2fe969e3a542815446ae8ca65
MassiveAtoms <s.ramsaransing.w@gmail.com>
2021-06-03 13:44:04 -0300

    made filter DRYer

--- library/forms.py
+++ library/forms.py
@@ -1,8 +1,8 @@
-# from crispy_forms.helper import FormHelper
-# from crispy_forms.layout import  Layout, Fieldset, Submit
-# from crispy_forms.bootstrap import Div, InlineField, InlineRadios
+import datetime
+
 from dal import autocomplete
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import *
 
@@ -13,14 +13,19 @@ AND_OR = [
 RATING_CHOICE = [(i, i) for i in range(11)]
 YEAR_CHOICE = [i for i in range(2022, 1970, -1)]
 
+
 class BookFilterForm(forms.Form):
+    """
+    form for advanced filtering, Booklist and various X_detail views
+    """
     authors = forms.ModelMultipleChoiceField(
         queryset=Author.objects.all(),
         widget=autocomplete.ModelSelect2Multiple(url='author-autocomplete'),
         required=False
     )
-    authors_andor = forms.ChoiceField(
+    authors_andor = forms.TypedChoiceField(
         label="author behaviour",
+        coerce=int,
         choices=AND_OR,
         initial=0,
         required=False,
@@ -32,8 +37,9 @@ class BookFilterForm(forms.Form):
             forward=['authors']),
         required=False
     )
-    tags_andor = forms.ChoiceField(
+    tags_andor = forms.TypedChoiceField(
         label="tags behaviour",
+        coerce=int,
         initial=0,
         choices=AND_OR, required=False,
         widget=forms.RadioSelect)
@@ -45,23 +51,57 @@ class BookFilterForm(forms.Form):
             forward=['authors', "tags"]),
         required=False
     )
-    series_andor = forms.ChoiceField(
+    series_andor = forms.TypedChoiceField(
         label="series behaviour",
         initial=0,
+        coerce=int,
         choices=AND_OR, required=False,
         widget=forms.RadioSelect)
 
-    langs = forms.ModelChoiceField(
+    languages = forms.ModelChoiceField(
         queryset=Language.objects.all(),
         widget=autocomplete.ModelSelect2(url='lang-autocomplete'),
         required=False
     )
-    rating_lte = forms.ChoiceField(choices=RATING_CHOICE, initial=RATING_CHOICE[0])
-    pubyear_begin = forms.IntegerField(required=False)
-    pubyear_end = forms.IntegerField(required=False)
-    timestamp_begin = forms.DateTimeField(required=False, widget=forms.SelectDateWidget(years=YEAR_CHOICE))
-    timestamp_end = forms.DateTimeField(required=False, widget=forms.SelectDateWidget(years=YEAR_CHOICE))
+    ratings__rating__gte = forms.TypedChoiceField(
+        label="rating",
+        choices=RATING_CHOICE,
+        coerce=int,
+        initial=RATING_CHOICE[0])
+    pubdate__gte = forms.IntegerField(label="smallest publishing year", required=False)
+    pubdate__lte = forms.IntegerField(label="biggest publishing year", required=False)
+    timestamp__gte = forms.DateTimeField(label="begin date range added to calibre", required=False,
+                                         widget=forms.SelectDateWidget(years=YEAR_CHOICE))
+    timestamp__lte = forms.DateTimeField(label="end date range added to calibre", required=False,
+                                         widget=forms.SelectDateWidget(years=YEAR_CHOICE))
 
+    def clean_pubdate__gte(self):
+        """
+        transform year integer to date object
+        :return: date object
+        """
+        data = self.cleaned_data['pubdate__gte']
+        if not data:
+            return None
+        try:
+            data = datetime.date(data, 1, 1)
+        except ValueError:
+            raise ValidationError("Invalid year.")
+        return data
+
+    def clean_pubdate__lte(self):
+        """
+         transform year integer to date object
+         :return: date object
+         """
+        data = self.cleaned_data['pubdate__lte']
+        if not data:
+            return None
+        try:
+            data = datetime.date(data, 12, 31)
+        except ValueError:
+            raise ValidationError("Invalid year.")
+        return data
 
 
 class SearchForm(forms.Form):
--- library/urls.py
+++ library/urls.py
@@ -7,7 +7,6 @@ from . import views
 logger = logging.getLogger(__name__)
 
 urlpatterns = [
-
     # Autocomplete "api" for filter form
     path("author-autocomplete/", views.AuthorComplete.as_view(), name="author-autocomplete"),
     path("tag-autocomplete/", views.TagComplete.as_view(), name="tag-autocomplete"),
@@ -15,19 +14,15 @@ urlpatterns = [
     path("series-autocomplete/", views.SeriesComplete.as_view(), name="series-autocomplete"),
     path("title-autocomplete/", views.TitleComplete.as_view(), name="title-autocomplete"),
 
+
     path('results/', views.SearchResultsView.as_view(), name='results'),
     path("filter/", views.FilterView.as_view(), name="filter"),
     path('books/', views.FilterView.as_view(), name='books'),
-    path("tag/<int:tag>", views.FilterView.as_view(), name="tag-detail-view"),
-    path("author/<int:author>", views.FilterView.as_view(), name="author-detail-view"),
-    path("series/<int:series_id>", views.FilterView.as_view(), name="series-detail-view"),
-    path("publisher/<int:publisher>", views.FilterView.as_view(), name="publisher-detail-view"),
-    path("rating/<int:rating>", views.FilterView.as_view(), name="publisher-detail-view"),
-
-
+    path("tag/<int:tags__id>", views.FilterView.as_view(), name="tag-detail-view"),
+    path("author/<int:authors__id>", views.FilterView.as_view(), name="author-detail-view"),
+    path("series/<int:series__id>", views.FilterView.as_view(), name="series-detail-view"),
+    path("publisher/<int:publishers>", views.FilterView.as_view(), name="publisher-detail-view"),
+    path("rating/<int:ratings__rating>", views.FilterView.as_view(), name="publisher-detail-view"),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail-view'),
-
-
-
 
 ]
