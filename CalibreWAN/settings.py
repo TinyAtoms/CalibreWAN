2fd8b445b4a98525664c2f4f3e05ea7e50af1d83
TinyAtoms <s.ramsaransing.w@gmail.com>
2022-04-11 21:02:49 -0300

    fix: :bug: removing redefinitions of csp header settings

--- CalibreWAN/settings.py
+++ CalibreWAN/settings.py
@@ -214,13 +214,11 @@ CSP_CONNECT_SRC = ("'self'",)
 CSP_OBJECT_SRC = ("'self'", )
 CSP_BASE_URI = ("'self'", )
 CSP_FRAME_ANCESTORS = ("'self'", )
-CSP_FORM_ACTION = ("'self'", )
 CSP_INCLUDE_NONCE_IN = ('script-src', )
 CSP_MANIFEST_SRC = ("'self'", )
 CSP_WORKER_SRC = ("'self'", )
 CSP_MEDIA_SRC = ("'self'", )
 CSP_FORM_ACTION = ("'self'", "github.com", "accounts.google.com")
-CSP_INCLUDE_NONCE_IN=['script-src']
 
 # SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = "Strict"
